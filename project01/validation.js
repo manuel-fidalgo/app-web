5f59355e2262b59b11a203386916b1024cf42c08
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2017-03-29 21:47:51 +0200

    Fixed some bugs, added the label click events

--- project01/validation.js
+++ project01/validation.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
-	//instead of .changed() 
-	$('.tipo1:first').click(function(){
+	//Clicks events for the items types
+	$('.tipo1:first').change(function(){
 		$('.tipo1').prop('checked', $('.tipo1:first').is(':checked'));
 	});
 
-	$('.tipo2:first').click(function(){
+	$('.tipo2:first').change(function(){
 		$('.tipo2').prop('checked', $('.tipo2:first').is(':checked'));
 	});
 
-	$('.tipo3:first').click(function(){
+	$('.tipo3:first').change(function(){
 		$('.tipo3').prop('checked', $('.tipo3:first').is(':checked'));
 	});
+
+	$('.cb').click(function() {
+    	var str = '[value="'+$(this).text()+'"]';
+		$(str).prop('checked', !$(str).is(':checked'));
+		$(str).trigger("change"); //forces the change event.
+    });
+
     //When any checkbox changes
     $('input:checkbox').change(function(){ //Writes all the checked checkboxes
     	$('#textarea').val("");
@@ -20,30 +27,19 @@ $(document).ready(function(){
     	else 
     		$('#textarea').show("fast");
     });
-
+    //Submits the form
     $("#submit-button").click(function(){
 
-    	if($("input.sb:checkbox").prop("checked")) //BUG HERE
-    		submitForm();
+    	if($('input.sb:checkbox:checked').length !=0)
+    		$('#form').submit();
     	else
     		alert("Debe selecionar almenos un item."); 
     });
+    
 });
-function submitForm() {
-
-	// body...
-}
 function getNameAllChecked() {
 	var array = $('input.sb:checkbox:checked').map(function(){
 		return $(this).val();
 	}).get();
 	return array.sort().join("\n");
 }
-
-/*
-if($('.tipo1:first').is(":checked")){
-	$('.tipo1').prop('checked', true);
-}else{
-	$('.tipo1').prop('checked', false);
-}
-*/
