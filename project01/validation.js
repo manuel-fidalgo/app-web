a66b6148d40c87a6633a936cef3bb3e395a1b807
Manuel Fidalgo <m.fidalgo.fierro@gmail.com>
2017-03-29 18:06:07 +0200

    Implemented the Javascript/Jquery

--- project01/validation.js
+++ project01/validation.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
 
 	//Also could we use .changed() instead of
-    $('.tipo1:first').click(function(){
-    	if($('.tipo1:first').is(":checked")){
-    		$('.tipo1').prop('checked', true);
-    	}else{
-    		$('.tipo1').prop('checked', false);
-    	}
-    });
+	$('.tipo1:first').click(function(){
+		if($('.tipo1:first').is(":checked")){
+			$('.tipo1').prop('checked', true);
+		}else{
+			$('.tipo1').prop('checked', false);
+		}
+	});
 
-    $('.tipo2:first').click(function(){
-    	if($('.tipo2:first').is(":checked")){
-    		$('.tipo2').prop('checked', true);
-    	}else{
-    		$('.tipo2').prop('checked', false);
-    	}
-    });
+	$('.tipo2:first').click(function(){
+		if($('.tipo2:first').is(":checked")){
+			$('.tipo2').prop('checked', true);
+		}else{
+			$('.tipo2').prop('checked', false);
+		}
+	});
 
-    $('.tipo3:first').click(function(){
-        if($('.tipo3:first').is(":checked")){
-    		$('.tipo3').prop('checked', true);
-    	}else{
-    		$('.tipo3').prop('checked', false);
-    	}
-    });
+	$('.tipo3:first').click(function(){
+		if($('.tipo3:first').is(":checked")){
+			$('.tipo3').prop('checked', true);
+		}else{
+			$('.tipo3').prop('checked', false);
+		}
+	});
     //When any checkbox changes
     $('input:checkbox').change(function(){ //Writes all the cheked checkboxes
     	$('#textarea').val("");
@@ -33,5 +33,8 @@ $(document).ready(function(){
 });
 
 function getNameAllChecked() {
-	return ["just","for","testing"];
+	var array = $('input.sb:checkbox:checked').map(function(){
+		return $(this).val();
+	}).get();
+	return array.sort();
 }
